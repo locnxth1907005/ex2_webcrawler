3aa4ef7d182752eb745fdd5ec418fb2d05fb1b1b
locnxth1907005 <locnxth1907005@fpt.edu.vn>
2021-01-15 21:14:43 +0700

    ok

---
+++ URLCrawler/Program.cs
@@ -0,0 +1,54 @@
+﻿using HtmlAgilityPack;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrawlUrlWeblAsync();
+            Console.ReadLine();
+        }
+
+        private static async Task CrawlUrlWeblAsync()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            var url = "https://vnexpress.net/suc-khoe/dinh-duong";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            var articles = htmlDocument.DocumentNode.Descendants("article").ToList();
+            foreach (var article in articles)
+            {
+                if (article.Descendants("a").FirstOrDefault() != null)
+                {
+                    var link = (string)article.Descendants("a").FirstOrDefault().ChildAttributes("href").FirstOrDefault().Value;
+                                      
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+
+
+                    var body = Encoding.UTF8.GetBytes(link);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "amqwebcrawler",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", link);
+                }
+            }
+        }
+    }
+}
+
